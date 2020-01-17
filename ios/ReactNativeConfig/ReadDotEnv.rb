3f09f43f8b56fbb5ba8aed61d07821cb3999a775
马超 <machao@seiue.com>
2020-01-17 16:31:25 +0800

    Fix: 只保留一级

--- ios/ReactNativeConfig/ReadDotEnv.rb
+++ ios/ReactNativeConfig/ReadDotEnv.rb
@@ -11,27 +11,18 @@ def get_env_files(envs_root, default_env_file)
   file_arr = default_env_file.split('.')
   env_file_name = ".#{file_arr[1]}.#{file_arr[2]}"
 
-  env_file = "#{envs_root}/#{env_file_name}"
-  local_env_file = "#{env_file}.local"
-
   root_env_file = "#{envs_root}/../../env/#{env_file_name}"
   local_root_env_file = "#{root_env_file}.local"
 
-  specified_env_file = ENV['ENVFILE']
-
   return [
-    local_env_file,
-    env_file,
     local_root_env_file,
     root_env_file
-  ].select{ |item|
-    specified_env_file ? item[item.length - specified_env_file.length, item.length] == specified_env_file : true
-  }
+  ]
 end
 
 # TODO: introduce a parameter which controls how to build relative path
 def read_dot_env(envs_root)
-  defaultEnvFile = '.env.development'
+  defaultEnvFile = ENV['ENVFILE'] || '.env.development'
   puts "going to read env file from root folder #{envs_root}"
 
   env_files = get_env_files(envs_root, defaultEnvFile)
